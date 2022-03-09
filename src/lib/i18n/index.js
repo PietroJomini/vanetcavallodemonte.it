277a5a0ccdeb13f7a4fffa96f28c13e863d37dcf
Pietro Jomini <pietro.jomini@protonmail.ch>
2022-03-09 23:39:08 +0100

    fallback locale

--- src/lib/i18n/index.js
+++ src/lib/i18n/index.js
@@ -2,15 +2,17 @@ import i18n from 'sveltekit-i18n';
 import { createHook } from './hook.js';
 import map from '/static/translations/locales.map.json';
 
+export const defaultLocale = 'it';
+export const exceptions = ['api', 'admin', 'maintenance'];
+
 /** @type {import('sveltekit-i18n').Config} */
 const config = {
 	loaders: map.loaders.map((l) => ({
 		...l,
 		loader: () => import(/* @vite-ignore */ l.loader)
-	}))
+	})),
+	fallbackLocale: defaultLocale
 };
 
-export const defaultLocale = 'it';
-export const exceptions = ['api', 'admin', 'maintenance'];
 export const { t, locale, locales, loading, loadTranslations } = new i18n(config);
 export const hook = createHook({ locales, defaultLocale, exceptions });
