6760e3e9ab1f669f77c00c1e30b4f44aa49d288d
Pietro Jomini <pietro.jomini@protonmail.ch>
2022-03-14 19:24:17 +0100

    disallow keys.json from vite serve

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
+import { defineConfig } from 'vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: defineConfig({ server: { fs: { deny: ['keys.json'] } } })
 	}
 };
 
